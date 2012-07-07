74f69924f6c90b23d38a767578a48572bcf642af
rch850 <rich850@gmail.com>
2012-07-07 11:30:43 +0900

    肉をクラスにしました。

--- main.js
+++ main.js
@@ -1,43 +1,44 @@
 enchant();
 
-window.onload = function() {
-    var game = new Game(320, 320);
+var Niku = enchant.Class.create(enchant.Sprite, {
+  initialize: function(x, y) {
+    enchant.Sprite.call(this, 32, 32);
+    this.image = game.assets["niku.png"];
+    this.x = x * 32;
+    this.y = y * 32;
+    this.type = Math.floor(Math.random() * 6);
+    this.frame = this.type;
 
+    this.addEventListener("touchend", function() {
+      var i;
+      tappedNiku[tappedNiku.length] = this;
+      // 前回と違う肉がタップされたらもとに戻す
+      if (tappedNiku.length >= 2 && tappedNiku[tappedNiku.length - 1].type !== tappedNiku[tappedNiku.length - 2].type) {
+        for (i = 0; i < tappedNiku.length; i++) {
+          tappedNiku[i].frame = tappedNiku[i].type;
+        }
+        tappedNiku = [];
+        return;
+      }
+      this.frame = 8;
+    });
+
+    game.rootScene.addChild(this);
+  }
+});
+
+window.onload = function() {
+    game = new Game(320, 320);
     game.preload("niku.png");
     game.onload = function() {
-      var nikuTable = [];
       var x, y;
-      var niku;
-      var tappedNiku = [];
+      tappedNiku = [];
+      nikuTable = [];
 
       for (y = 0; y < 6; y++) {
         nikuTable[y] = [];
         for (x = 0; x < 6; x++) {
-
-          niku = new Sprite(32, 32);
-          niku.image = game.assets["niku.png"];
-          niku.x = x * 32;
-          niku.y = y * 32;
-          niku.type = Math.floor(Math.random() * 6);
-          niku.frame = niku.type;
-
-          niku.addEventListener("touchend", function() {
-            var i;
-            tappedNiku[tappedNiku.length] = this;
-            // 前回と違う肉がタップされたらもとに戻す
-            if (tappedNiku.length >= 2 && tappedNiku[tappedNiku.length - 1].type !== tappedNiku[tappedNiku.length - 2].type) {
-              for (i = 0; i < tappedNiku.length; i++) {
-                console.log("revert niku: "+ i);
-                tappedNiku[i].frame = tappedNiku[i].type;
-              }
-              tappedNiku = [];
-              return;
-            }
-            this.frame = 8;
-          });
-
-          game.rootScene.addChild(niku);
-          nikuTable[y][x] = niku;
+          nikuTable[y][x] = new Niku(x, y);
         }
       }
     }
